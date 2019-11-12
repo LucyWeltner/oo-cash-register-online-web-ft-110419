52187d4d25c70690d7aeefe146793ef80fe4e983
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-12 18:04:37 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,5 +7,12 @@ class CashRegister
   
   def add_item(item, price, quantity = 1)
     @total += price*quantity
+    @total
   end 
+  
+  def apply_discount
+    @total = @total * (1 - discount/100)
+    @total
+  end 
+  
 end
